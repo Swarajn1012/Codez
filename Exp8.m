7652907273e1899879416517c1cf6eb1d3afe1e1
Swarajn1012 <swarajnangare36@gmail.com>
2024-11-11 18:23:08 +0530

    Add files via upload

---
+++ Exp8.m
@@ -0,0 +1,55 @@
+﻿clc;
+clear all;
+close all;
+
+% Input parameters
+n = input('Enter the value of n: ');
+k = input('Enter the value of k: ');
+m = n - k;
+
+% Generate generator polynomial for the cyclic code
+G = cyclpoly(n, k, 'max');
+disp('Generator polynomial:');
+disp(poly2sym(G));
+
+% Generate codewords using the generator polynomial
+d1 = [1 0 0 0];
+c1 = poly2sym(d1) * poly2sym(G);
+d2 = [0 1 0 0];
+c2 = poly2sym(d2) * poly2sym(G);
+d3 = [0 0 1 0];
+c3 = poly2sym(d3) * poly2sym(G);
+d4 = [0 0 0 1];
+c4 = poly2sym(d4) * poly2sym(G);
+
+% Display codewords and data vectors
+disp('Data vectors:');
+d = [d1; d2; d3; d4]
+disp('Codewords:');
+s = [c1; c2; c3; c4]
+
+% Generate parity-check matrix and syndrome table for Hamming code
+parmat = hammgen(m);
+trt = syndtable(parmat);
+
+% First received codeword
+recd = [0 1 0 1 0 0 0];
+syndrome = rem(recd * parmat', 2);
+syndrome_de = bi2de(syndrome, 'left-msb');
+disp(['Syndrome (decimal): ', num2str(syndrome_de)]);
+disp(['Syndrome (binary): ', num2str(syndrome)]);
+Error = trt(1 + syndrome_de, :);
+corrected_code = rem(Error + recd, 2);
+disp('Corrected code for first received word:');
+disp(corrected_code);
+
+% Second received codeword
+recd = [1 1 0 1 1 0 1];
+syndrome = rem(recd * parmat', 2);
+syndrome_de = bi2de(syndrome, 'left-msb');
+disp(['Syndrome (decimal): ', num2str(syndrome_de)]);
+disp(['Syndrome (binary): ', num2str(syndrome)]);
+Error = trt(1 + syndrome_de, :);
+corrected_code = rem(Error + recd, 2);
+disp('Corrected code for second received word:');
+disp(corrected_code);
---
+++ Exp9.m
@@ -0,0 +1,69 @@
+﻿clc;
+clear all;
+
+% Input generator matrix
+g = input('Enter the generator matrix: ');
+disp('G =');
+disp(g);
+
+% Determine the order of the linear block code
+[n, k] = size(transpose(g));
+disp('The order of the linear block code for given generator matrix is: ');
+disp(['n = ', num2str(n), ', k = ', num2str(k)]);
+
+% Generate all possible codewords
+u = zeros(2^k, k);
+for i = 1:2^k
+    for j = k:-1:1
+        if rem(i - 1, 2^(-j + k + 1)) >= 2^(-j + k)
+            u(i, j) = 1;
+        else
+            u(i, j) = 0;
+        end
+    end
+end
+
+% Calculate codewords by multiplying message vectors with G
+c = rem(u * g, 2);
+disp('The possible codewords are: ');
+disp(c);
+
+% Calculate minimum Hamming distance
+disp('The minimum Hamming distance dmin for given block code = ');
+d_min = min(sum(c(2:2^k, :)')); % Exclude the all-zero codeword
+disp(d_min);
+
+% Input received codeword
+r = input('Enter the received code word: ');
+
+% Calculate the parity-check matrix H
+p = g(:, n - k + 1:n);
+h = [transpose(p), eye(n - k)];
+disp('Parity-check matrix H: ');
+disp(h);
+
+% Calculate syndrome of received codeword
+ht = transpose(h);
+s = rem(r * ht, 2);
+disp('Syndrome of the given codeword is: ');
+disp(s);
+
+% Error correction based on syndrome
+error_pos = -1;
+for i = 1:n
+    if isequal(ht(i, :), s)
+        error_pos = i;
+        r(i) = 1 - r(i);  % Flip the bit at the error position
+        break;
+    end
+end
+
+% Display results
+if error_pos > 0
+    disp(['The error is in bit: ', num2str(error_pos)]);
+else
+    disp('No single-bit error detected');
+end
+
+disp('The corrected codeword is: ');
+disp(r);
